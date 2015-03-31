78d9c0e8042735612f45c9e5463169dc6aed72bb
yuristefani <yuri@verdeal.com.br>
2015-03-31 13:28:34 -0300

    jogo
    1

---
+++ untitled22.py
@@ -0,0 +1,7 @@
+#YURI NAGHIRNIAC DE STEFANI
+palavras=list()#todas as palavras da lista
+lixo=list()#para onde ficas as palavras usadas
+atual2=list()#lista formada com as letras da palavra
+formado=list()#a palavra atual com as letras usadas
+posicao=list()#local do gráfico para colocar a letra
+usada=list()#letras usadas
